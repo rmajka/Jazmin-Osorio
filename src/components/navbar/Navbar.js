bbf39647a94150cf2a1dcd54945b6f24d6cfdb1e
Rafal <rafalpawel.majka@.com>
2023-06-04 22:12:31 +0200

    add border bottom to active link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,52 @@
 import styles from "./Navbar.module.css";
 import SocialMediaBox from "../socialmediabox/SocialMediaBox";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [activeLink, setActiveLink] = useState({
+    home: false,
+    sobremi: false,
+    articulos: false,
+  });
+
   return (
     <nav className={styles.navContainer}>
       <div className={styles.leftSide}>
         <h4 className={styles.name}>Jazmin Osorio</h4>
         <ul className={styles.listContainer}>
           <li>
-            <a href="#home">Home</a>
+            <a
+              className={activeLink.home ? styles.active : styles.link}
+              onClick={() =>
+                setActiveLink({ home: true, sobremi: false, articulos: false })
+              }
+              href="#home"
+              on
+            >
+              Home
+            </a>
           </li>
           <li>
-            <a href="#sobremi">Sobre mí</a>
+            <a
+              className={activeLink.sobremi ? styles.active : styles.link}
+              onClick={() =>
+                setActiveLink({ home: false, sobremi: true, articulos: false })
+              }
+              href="#sobremi"
+            >
+              Sobre mí
+            </a>
           </li>
           <li>
-            <a href="#articulos">Artículos</a>
+            <a
+              className={activeLink.articulos ? styles.active : styles.link}
+              onClick={() =>
+                setActiveLink({ home: false, sobremi: false, articulos: true })
+              }
+              href="#articulos"
+            >
+              Artículos
+            </a>
           </li>
         </ul>
       </div>
